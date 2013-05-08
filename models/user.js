f43de80f1fc88a3bccce641052722015a5f28913
osada hiroshi <higehossa@gmail.com>
2013-05-08 20:54:45 +0900

    書き途中だった。 一旦、完了。

--- models/user.js
+++ models/user.js
@@ -1,9 +1,32 @@
 user.authenticate = function(name, password, callback){
 	db.query('SELECT * FROM users WHERE name = ?',
 		[name], queryCallback);
+	function queryCallback(err, results, fields){
+		db.end();
+		if(err){
+			callback(err);
+			return;
+		}
+		if(results && (results.length > 0)){
+			userInfo = results[0];
+			if(userInfo.password == _hashPassword(password)){
+				delete userInfo.password;
+				callback(null, userInfo);
+				return;
+			}
+		}
+		callback(err, null);
+		return;
+	}
 
 
-	// テスト書き込み。
+	function _hashPassword(password){
+		if(password === ''){
+			return '';
+		}
+		var shasum = crypto.createHash('sha256');
+		shasum.update(password);
+		return shasum.digest('hex');
+	}
 
-	
 };
